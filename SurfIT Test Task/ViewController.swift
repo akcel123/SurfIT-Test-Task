d6b805fecc318cadff1091843816580d17bfe58e
Denis Pavlov <akcel322@gmail.com>
2023-06-01 12:01:36 +0700

    Initial Commit

---
+++ SurfIT Test Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfIT Test Task
+//
+//  Created by Денис Павлов on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
